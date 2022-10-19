1d1fa7896ef40311b53003ee435dc472467d0bf9
Felipe Bazzotti <felipebazzotti@outlook.com>
2022-10-18 21:15:45 -0300

    JavaScript: Programando na linguagem da web. Aula 08.Filtrando uma tabela

---
+++ HTML e CSS/JAVASCRIPT PROGRAMANDO NA LINGUAGEM DA WEB/js/filtra.js
@@ -0,0 +1,24 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+
+campoFiltro.addEventListener("input", function() {
+    var pacientes = document.querySelectorAll(".paciente");
+
+    if(this.value.length > 0) {
+        for(var i = 0; i< pacientes.length; i++){
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            var experssao = new RegExp(this.value,"i");
+            if( !experssao.test(nome)){
+                paciente.classList.add("invisivel");
+            }else {
+                paciente.classList.remove("invisivel");
+            }
+        }
+    }else {
+        for(var i = 0; i< pacientes.length; i++){
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+});
